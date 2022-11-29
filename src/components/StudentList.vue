4969e13d1e0cdbfb152608ba9e925e32a2a7c6a0
Geri0326 <79844316+Geri0326@users.noreply.github.com>
2022-11-29 11:31:41 +0100

    ok

---
+++ src/components/StudentList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {ref} from "vue";
+const studentList = ref();
+onMounterd(async () => {
+  const response = await fetch("https://pb.seiwald.club/api/collections/4bhk/records")
+    const data = await response.json();
+    studentList.value = data.records;
+}
+
+);
+</script>
+
+<template>
+    <h1>Our Students</h1>
+    
+    <ul>
+        <li v-for="student in studentList" >
+            {{ student.name }}
+        </li>
+    </ul>
+</template>
