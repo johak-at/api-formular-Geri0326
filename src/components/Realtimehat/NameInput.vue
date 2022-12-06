fbedb3283fa59f2073c8548bd5304db36feb2c52
Geri0326 <79844316+Geri0326@users.noreply.github.com>
2022-12-06 11:57:48 +0100

    b

---
+++ src/components/Realtimehat/NameInput.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref} from "vue";
+const emits = defineEmits(["setUserName"]);
+const userName = ref("");
+
+</script>
+
+<template>
+    <h2>Hello Friend! What's your name?</h2>
+    <input v-model="userName" type="text" placeholder="Enter Username" @keyup.enter="emits('setUserName', userName)"> &nbsp;
+    <button @click="emits('setUserName', userName)">Set Username</button>
+</template>
---
+++ src/components/Realtimehat/RealtimeChat.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+import NameInput from "./NameInput.vue";
+const userName = ref();
+function setUserName(name) {
+    userName.value = name;
+}
+</script>
+
+<template>
+    <NameInput @set-user-name="setUserName" v-if="!userName"></NameInput>	
+    <div v-else>
+        <h1>Hi {{userName}}!</h1>
+    </div>
+</template>
